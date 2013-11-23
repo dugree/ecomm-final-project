736017b409d6866dc34fce50c9f9b9f31359426f
Dustin Green <dustingreen987@gmail.com>
2013-11-23 13:08:11 -0600

    added image upload abilities

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Product do
-
+  form do |f|
+    f.inputs "details" do
+      f.input :category
+      f.input :name
+      f.input :price
+      f.input :stock
+    end
+    f.inputs "image" do
+      f.input :image, :as => :file
+    end
+    f.actions
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category_id, :description, :name, :price, :stock
-
+  attr_accessible :category_id, :description, :name, :price, :stock, :image
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates :category_id, :name, :price, :stock,   :presence => true
   validates :name,                                :uniqueness => true
   validates :price,                               :numericality => { :greater_than_or_equal_to => 0.01 }
