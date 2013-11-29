c3f94e25e55a6a05f02628743a584770f689583e
Dustin Green <dustingreen987@gmail.com>
2013-11-28 20:33:14 -0600

    added admin ability to list outstanding orders by customer

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,3 +1,34 @@
 ActiveAdmin.register Customer do
-
+  show do
+    panel "Details" do
+      div do
+        customer.name
+      end
+      div do
+        customer.address
+      end
+      div do
+        customer.postal_code
+      end
+      div do
+        customer.city
+      end
+      div do
+        customer.email
+      end
+      div do
+        customer.province.name
+      end
+    end
+    panel "Outstanding Orders" do
+      table_for customer.orders.where(:status_id => 2) do
+        column "Order Number" do |order|
+          link_to order.id, admin_order_path(order)
+        end
+        column "Order Date" do |order|
+          order.created_at
+        end
+      end
+    end
+  end
 end
